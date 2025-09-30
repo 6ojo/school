07b91a9127b9ceb0d8e035bff53846db849a2d73
gojo <github@gojo.mozmail.com>
2025-09-29 21:05:50 -0700

    its a function

--- nestedStringPractice.js
+++ nestedStringPractice.js
@@ -1,8 +1,8 @@
 function getRandomInteger(min, max)
 {
-    let range = max - min;
-    let rand = Math.floor(Math.random() * range);
-    return min + rand;
+    let range = max - min + 1
+    let rand = Math.floor(Math.random() * range)
+    return min + rand
 }
 
 //Create a function following this contract:
@@ -10,21 +10,20 @@ function getRandomInteger(min, max)
 //Purpose: takes a number between 1 and 8. If first place, say gold medal! If second or third, say made podium! Otherwise say not this time...
 //Input: number between 1 and 8
 //Output: String depeding on input number
+function awardMedals(place) {
+    if (place === 1) {
+        return "gold medal!"
+    } else if (place <= 3) {
+        return "made podium!"
+    } else {
+        return "not this time... :("
+    }
+}
 
-function awardMedals(place)
-let place = getRandomInteger(1,8)
-console.log(place)
-if (place == 1){
-    console.log("you win!")
-}
-else{
-    if (place <= 3){
-        console.log("made podium!")
-    }
-    else {
-        console.log("try again :(")
-    }
-}
 //Create a program that:
 //creates a random number between 1 and 8
+let place = getRandomInteger(1, 8)
 //prints the result of the awardMedals function with the number created
+console.log(`Your place: ${place}`)
+let result = awardMedals(place);
+console.log(result);
